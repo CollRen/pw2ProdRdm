51d0f6abdf5f263d5eb6ae245d7d2fb911a18c81
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-06-25 14:23:19 -0400

    commit initial

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PORT: 3306,
+    JWT_SECRET: "shhh",
+    HOST: 'localhost',
+    USER: "root",
+    PASSWORD: "root",
+    DB: "bobazou",
+    dialect: 'mysql'
+}
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const corsOption = {
+    origin: '*'
+}
+require('dotenv').config()
+console.log(process.env)
+
+app.use(cors(corsOption))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const db = require('./app/models')
+db.connex.sync()
+//test
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome' })
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
+
+require('./app/routes/utilisateur/utilisateur.routes')(app);
+require('./app/routes/privilege/privilege.routes')(app);
+require('./app/routes/ville/ville.routes')(app);
+require('./app/routes/province/province.routes')(app);
+require('./app/routes/statut/statut.routes')(app);
+require('./app/routes/expedition/expedition.routes')(app);
+require('./app/routes/corp/corp.routes')(app);
+require('./app/routes/carburant/carburant.routes')(app);
+require('./app/routes/motopropulseur/motopropulseur.routes')(app);
+require('./app/routes/transmission/transmission.routes')(app);
+require('./app/routes/constructeur/constructeur.routes')(app);
+require('./app/routes/model/model.routes')(app);
+require('./app/routes/image/image.routes')(app);
+require('./app/routes/mode_paiement/mode_paiement.routes')(app);
+require('./app/routes/voiture/voiture.routes')(app);
+require('./app/routes/commande/commande.routes')(app);
+
+
